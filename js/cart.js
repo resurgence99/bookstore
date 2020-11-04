7933eb9901f8de9131efe3517ece0ea1d72f6d9f
Ganesh G <52821939+resurgence99@users.noreply.github.com>
2020-11-04 22:08:05 +0530

    all the cart options in index is working

--- js/cart.js
+++ js/cart.js
@@ -48,6 +48,7 @@ let products = [
 for(let i=0;i<carts.length;i++){
     carts[i].addEventListener('click',()=>{
         cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
@@ -107,6 +108,18 @@ function setItems(product){
     localStorage.setItem("productsInCart",JSON.stringify(cartItems));   
     }
     
+function totalCost(product){
+    
+    let cartCost = localStorage.getItem('totalCost');
+    
+    if(cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }
+    else{
+    localStorage.setItem("totalCost", product.price);
+    }
+}
 
 
 onLoadCartNumbers();
